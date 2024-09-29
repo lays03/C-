2b6201715b166a640848847d22c52180655bb2c1
lays03 <2725281280@qq.com>
2024-09-29 17:48:35 +0800

    09.29

--- hello.cpp
+++ hello.cpp
@@ -2,83 +2,39 @@
 #include<string>
 using namespace std;
 
-/*
-两种分类方式：<br>
-- 按参数分为：有参构造和无参构造
-- 按类型分为：普通构造和拷贝构造
-*/
-class Person
-{
-private:
-    /* data */
-
+class Phone{
 public:
-    int age;
-    //构造函数
-    Person() //无参构造（默认构造）
-    {
-        cout << "Person的无参构造函数调用" << endl;
+    string m_pname; //手机名字
+    Phone(string name){
+        cout << "Phone的构造函数调用" << endl;
+        m_pname = name;
     }
-    Person(int a)
-    {
-        age = a;
-        cout << "Person的有参构造函数调用" << endl;
-    }
-    //拷贝构造函数 
-    //因为需要拷贝传进来的数据，因此改数据不能更改，因此加上const，并以引用的方式
-    Person(const Person &p){
-        //将传入的人身上的属性，拷贝到我身上
-        age = p.age;
-        cout << "Person的拷贝构造函数调用" << endl;
-    }
-
-    ~Person()
-    {
-        cout << "Person的析构函数调用" << endl;
+    ~Phone(){
+        cout << "Phone的析构函数调用" << endl;
     }
 };
 
-//调用
-void test01(){
-    //1、括号法
-    // Person p1; //默认构造函数调用
-    // Person p2(10);
-    // Person p3(p2);
-    // // cout << "p1的年龄为：" << p1.age << endl;
-    // // cout << "p2的年龄为：" << p2.age << endl;
-    // // cout << "p2的年龄为：" << p3.age << endl;
-    // //注意事项
-    // //调用默认构造函数时候，不要加()
-    // Person P(); //不会调用构造函数，因为编译器会认为是一个函数的声明
+class Person{
+public:
+    //姓名
+    string m_name;
+    //手机
+    Phone m_phone;
     
-    //2、显示法
-    // Person p1;
-    // Person p2 = Person(10); //有参构造
-    // //拷贝构造函数
-    // Person p3 = Person(p2);
-    // //等号的右边 Person(10); 被叫做“匿名对象”，
-    // //特点：当前行执行结束后，系统会立即回收掉匿名对象
-    // Person(10); //匿名对象
-    // cout << "aaa" << endl;
-/*
-可以看到，Person(10)调用的是有参构造函数，并且执行完这一行之后，立马调用析构函数
-Person的无参构造函数调用
-Person的有参构造函数调用
-Person的拷贝构造函数调用
-Person的有参构造函数调用
-Person的析构函数调用
-aaa
-Person的析构函数调用
-Person的析构函数调用
-Person的析构函数调用
-*/
-    //注意事项
-    //不要利用拷贝构造函数 初始化匿名对象
-    // Person(p3); 错误，编译器会认为Person(p3) === Person p3，会导致p3重定义
+    //这里的m_phone(name) 相当于 Phone m_phone = pname 隐式转换法
+    Person(string name, string pname):m_name(name), m_phone(pname){
+        cout << "Person的构造函数调用" << endl;
+    }
+    ~Person(){
+        cout << "Person的析构函数调用" << endl;
+    }
 
-    //3、隐式转换法
-    Person p4 = 10; //相当于Person p4 = Person(10);
-    Person p5 = p4; //隐式转换法的拷贝构造函数调用
+};
+//当其他类对象作为本类成员，构造时候先构造类对象，再构造自身
+//析构的顺序与之相反
+void test01(){
+    Person p1("liujing", "iphone15");
+    cout << p1.m_name << "的手机是" << p1.m_phone.m_pname << endl;
 }
 
 int main(){
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,6 @@
-    Person p5 = p4;
+//传统初始化操作
+    // Person(int age, int height){
+    //     m_age = age;
+    //     m_height = height;
+    //     cout << "Person有参构造函数调用" << endl;
+    // }
