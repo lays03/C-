12c20e1935fe1f709c8fbbc078a18e047b94b1be
qq_42494870 <qq_42494870@noreply.gitcode.com>
2025-03-12 16:26:25 +0800

    03.12

--- 1.cpp
+++ 1.cpp
@@ -1,97 +1,45 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <deque>
-#include <algorithm>
-#include <stack>
-#include <queue>
-#include <list>
-#include <map>
-
-#include <ctime>
 using namespace std;
-
-class Person
-{
-public:
-    string m_Name;
-    int m_Score;
-    Person(string name, int score)
-    {
-        this->m_Name = name;
-        this->m_Score = score;
-    }
-};
-
-//打印deque
-void printDeque(const deque<int>& d)
-{
-    for(deque<int>::const_iterator it = d.begin(); it != d.end(); ++it)
-    {
-        cout << *it << " ";
-    }
-    cout << endl;
-}
-
-
-//添加选手
-void addPerson(vector<Person>& v)
-{
-    string nameside = "ABCDE";
-    for(int i = 0; i < 5; ++i)
-    {
-        string name = "选手_";
-        name += nameside[i];
-        Person p(name, 0);
-        v.push_back(p);
-    }
-}
-
-//打分
-void setScore(vector<Person>& v)
-{
-    srand((unsigned)time(NULL));
-    for(vector<Person>::iterator it = v.begin(); it != v.end(); ++it)
-    {
-        deque<int> d;
-        int score = 60 + rand() % 41;
-        for(int i = 0; i < 10; ++i)
-        {
-            int score = 60 + rand() % 41;
-            d.push_back(score);
-        }
-        cout << it->m_Name << "得分排序前: " << endl;
-        printDeque(d);
-        //排序
-        sort(d.begin(), d.end());
-        cout << it->m_Name << "得分排序后: " << endl;
-        printDeque(d);
-
-        d.pop_back(); //去掉最高分
-        d.pop_front(); //去掉最低分
-        cout << it->m_Name << "去掉最高分和最低分后: " << endl;
-        printDeque(d);
-
-        int res = 0;
-        for(deque<int>::iterator dit = d.begin(); dit != d.end(); ++dit)
-        {
-            res += *dit;
-        }
-        res /= d.size(); //算出平均分
-        it->m_Score = res;
-        cout << it->m_Name << "的最终得分是: " << it->m_Score << endl;
-    }
-}
-
-
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+#include <stack>
+#include <map>
+map<char, int> m;
 
 int main()
 {
-    //添加选手
-    vector<Person> v;
-    addPerson(v);
+    m.insert(make_pair('0', 0));
+    m.insert(make_pair('1', 1));
+    m.insert(make_pair('2', 2));
+    m.insert(make_pair('3', 3));
+    m.insert(make_pair('4', 4));
+    m.insert(make_pair('5', 5));
+    m.insert(make_pair('6', 6));
+    m.insert(make_pair('7', 7));
+    m.insert(make_pair('8', 8));
+    m.insert(make_pair('9', 9));
+    m.insert(make_pair('A', 10));
+    m.insert(make_pair('B', 11));
+    m.insert(make_pair('C', 12));
+    m.insert(make_pair('D', 13));
+    m.insert(make_pair('E', 14));
+    m.insert(make_pair('F', 15));
 
-    //打分
-    deque<int> d;
-    setScore(v);
+    string str;
+    while(cin >> str)
+    {
+        int ans = 0;
+        int len = str.size();
+        for(int i = 2; i < len; ++i)
+        {
+            map<char, int>::iterator it = m.find(str[i]);
+            ans += (*it).second * pow(16, len - 1 - i);
+        }
+        cout << ans << endl;
+    }
+    
+
+    
+    
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,15 +1,2 @@
-if(m[(*it).first] == "策划")
-        // {
-        //     cout << "key = " << m[(*it).first] << " 姓名: " << (*it).second.m_Name
-        //         << " 工资: " << (*it).second.m_Money << endl;
-        // }
-        // else if(m[(*it).first] == "美术")
-        // {
-        //     cout << "key = " << m[(*it).first] << " 姓名: " << (*it).second.m_Name
-        //         << " 工资: " << (*it).second.m_Money << endl;
-        // }
-        // else
-        // {
-        //     cout << "key = " << m[(*it).first] << " 姓名: " << (*it).second.m_Name
-        //         << " 工资: " << (*it).second.m_Money << endl;
-        // }
+
+        }
